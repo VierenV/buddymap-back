d942524227096c3a4e7576c597abca560ef31335
VierenV <valentin.vieren@gmail.com>
2014-07-09 13:47:41 +0200

    Changement emplacement web xml Creation config jersey

---
+++ buddymap-back/src/com/buddymap/config/BuddyMap.java
@@ -0,0 +1,20 @@
+package com.buddymap.config;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.buddymap.resources.filters.RequestFilter;
+import com.buddymap.resources.filters.ResponseFilter;
+
+@ApplicationPath("/")
+public class BuddyMap extends ResourceConfig {
+	 
+	    public BuddyMap() {
+	        // Register resources and providers using package-scanning.
+	        packages("com.buddymap.resources");
+	        
+	        register(RequestFilter.class);
+	        register(ResponseFilter.class);
+	    }
+}
